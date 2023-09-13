530e268606b8e3ea815f6569df926521523a97f2
Alan <alan_soares300@hotmail.com>
2023-09-13 19:20:41 -0300

    add actions

--- src/FocusTimer/events.js
+++ src/FocusTimer/events.js
@@ -1,12 +1,10 @@
 import {control} from "./elements.js"
-
+import * as actions from "./actions.js"
 export function registerControls() {
     control.addEventListener('click', (event) => {
         const action = event.target.dataset.action
         if (action == undefined) {
             return
         }
-
-        console.log(action)
     })
 }
